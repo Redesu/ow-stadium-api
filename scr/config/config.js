6ad1c0137612a5a21bf955c392ff04fe51c12f44
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-10-19 20:40:52 -0300

    Initial commit

---
+++ scr/config/config.js
@@ -0,0 +1,13 @@
+import 'dotenv/config.js'
+
+const config = {
+    port: process.env.PORT || 3000,
+    env: process.env.NODE_ENV,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+    dbHost: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT
+}
+
+export default config
