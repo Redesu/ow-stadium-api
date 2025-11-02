d7826a48f3b7da2d59aaf8757db90011394cdbeb
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-02 19:33:58 -0300

    Adds api limiter to all routes, adds trusted ips to skip limiter

--- src/app.js
+++ src/app.js
@@ -9,9 +9,15 @@ import { swaggerSpec } from '../swagger.js';
 import radisClient from './config/redis.js'
 import rateLimit from 'express-rate-limit';
 
+
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 50,
+    skip: (req) => {
+        const trustedIps = process.env.TRUSTED_IPS ? process.env.TRUSTED_IPS.split(',') : [];
+        const isWhitelisted = trustedIps.includes(req.ip);
+        return isWhitelisted;
+    },
     message: 'Too many requests from this IP, please try again after 15 minutes',
     standardHeaders: true,
     legacyHeaders: false,
@@ -24,7 +30,7 @@ app.set('trust proxy', 2)
 
 app.use(express.json());
 
-app.use('/api/', apiLimiter);
+app.use(apiLimiter);
 app.use('/api/heroes', heroesRouter);
 app.use('/api/items', itemsRouter);
 app.use('/api/powers', powersRouter);
