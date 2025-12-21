7bf537cb08e02272feedf7f779d53a6538eefc49
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-21 19:46:15 -0300

    Updates redis initialization
    Update it so it only connects if REDIS_URL is set at environment

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,19 +1,21 @@
 import { createClient } from "redis";
 
 const client = createClient({
-    url: process.env.REDIS_URL || 'redis://localhost:6379'
-})
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
 
 let errorLogged = false;
 
-client.on('error', err => {
-    if (!errorLogged) {
-        console.error('Redis Client Error', err);
-        errorLogged = true;
-    }
+client.on("error", (err) => {
+  if (!errorLogged) {
+    console.error("Redis Client Error", err);
+    errorLogged = true;
+  }
 });
-client.on('connect', () => console.log('Redis Client Connected'));
+client.on("connect", () => console.log("Redis Client Connected"));
 
-client.connect();
+if (process.env.REDIS_URL) {
+  client.connect();
+}
 
-export default client
+export default client;
