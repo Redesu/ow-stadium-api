ac1663e8cf882cab8af344dfc8e04c6a55dfc818
Willyan David <62126803+Redesu@users.noreply.github.com>
2026-01-08 12:59:51 -0300

    Search swagger documentation
    Adds more information to swagger documentation, and fixes identation.

--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -8,22 +8,25 @@ const router = Router();
  * @swagger
  * /api/search:
  *   get:
- *    summary: Search entities (across items, items stats, heroes, powers)
- *    description: Search for entities across multiple collections including items, item stats, heroes, and powers. You can search by name, type, role, description, and specific item stats abbreviations.
- *    tags: [Search]
- *   parameters:
- *     - in: query
- *       name: term
- *   schema:
- *       type: string
- *   required: true
- *   responses:
- *     200:
- *       description: Search results found
- *     400:
- *       description: No search parameters provided
- *     404:
- *       description: Search results not found
+ *     summary: Search entities (across items, items stats, heroes, powers)
+ *     description: Search for entities across multiple collections including items, item stats, heroes, and powers. You can search by name, type, role, description, and specific item stats abbreviations.
+ *     tags:
+ *       - Search
+ *     parameters:
+ *       - in: query
+ *         name: term
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Comma-separated search terms or stat abbreviations (e.g. "ap, wp")
+ *         example: "ap"
+ *     responses:
+ *       200:
+ *         description: Search results found
+ *       400:
+ *         description: No search parameters provided
+ *       404:
+ *         description: Search results not found
  */
 router.get("/", cacheMiddleware, search);
 
